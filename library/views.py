496ae2423b6e8eddaccd7cb5b790edb0d5f67578
Kiptoo <denysmutay@gmail.com>
2024-02-26 15:03:16 +0200

    added view function for create_user and success page

--- library/views.py
+++ library/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import response
+from .forms import UserForm
 
 # Create your views here.
 def landing(request):
     return render(request,'landing.html')
+
+
+def create_user(request):
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = UserForm()
+    return render(request, 'createuser.html', {'form': form})
+
+def success(request):
+    return render(request,'success.html')
