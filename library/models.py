27030673e109e8e56fd72c4e41a0bcff32736b8f
Kiptoo <denysmutay@gmail.com>
2024-02-26 10:36:34 +0200

    added models for user account creation

--- library/models.py
+++ library/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    user_name = models.CharField(max_length=20)
+    email = models.EmailField(max_length=254)
+    bio = models.TextField((""))
+    last_seen = models.DateTimeField((""), auto_now=False, auto_now_add=True)
